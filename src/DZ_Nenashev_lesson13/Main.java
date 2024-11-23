1db45f4274bd8dce8381ef9784479fc2c29fe5fe
BlokadaVN <barracudavnk@gmail.com>
2024-11-23 10:20:36 +0700

    DZ

--- src/DZ_Nenashev_lesson13/Main.java
+++ src/DZ_Nenashev_lesson13/Main.java
@@ -20,7 +20,5 @@ public class Main {
             sum++;
         }
         return sum;
-
-
     }
 }
---
+++ src/DZ_Nenashev_lesson14/Main.java
@@ -0,0 +1,9 @@
+package DZ_Nenashev_lesson14;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Домашнее задание.");
+
+    }
+}
