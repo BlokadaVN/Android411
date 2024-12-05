4911c0c58e0673fac441cfcd1a439567d2edddf8
BlokadaVN <barracudavnk@gmail.com>
2024-12-05 21:18:48 +0700

    DZ17

--- src/DZ_Nenashev_lesson17/Main.java
+++ src/DZ_Nenashev_lesson17/Main.java
@@ -9,16 +9,18 @@ public class Main {
 
         String s = "Текст с повторяющимися символами";
         System.out.println("Исходная строка: " + s);
+      
         String a = " ";
         String b = "";
         s.contains(a);
         s = s.replace(a, b);
+
         StringBuilder sb = new StringBuilder();
 
 
         for (int i = 0; i < s.length(); i++) {
 
-            if (sb.indexOf(String.valueOf(s.charAt(i))) == -1) { // в цикле перебрали строку и при помощи .valueOf преобразовали строку в символы
+            if (sb.indexOf(String.valueOf(s.charAt(i))) == -1) { // в цикле перебрали строку и при помощи .valueOf преобразовали символы в строку.
 
                 sb.append(s.charAt(i));
             }
