6284c6633f690be16622b77c847fc6b9c6ece1dd
BlokadaVN <barracudavnk@gmail.com>
2025-02-12 11:11:43 +0700

    oop15

---
+++ src/DZoop15_Nenashev/Main.java
@@ -0,0 +1,23 @@
+package DZoop15_Nenashev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+//        Map<String, Integer> hashMapOld = new HashMap<>(); // Не работает.
+        Map<String, String> hashMapOld = new HashMap<>();
+        hashMapOld.put("X", "1");
+        hashMapOld.put("Y", "2");
+        hashMapOld.put("Z", "3");
+        Map<String, String> hashMapNew = new HashMap<>();
+
+        for (var entry : hashMapOld.entrySet()) {
+//            hashMapNew.put(entry.getValue(), entry.getKey()); // Не работает.
+            hashMapNew.put(entry.getValue(), entry.getKey());
+        }
+
+        System.out.println("Исходный Map: " + hashMapOld);
+        System.out.println("Измененный Map: " + hashMapNew);
+    }
+}
