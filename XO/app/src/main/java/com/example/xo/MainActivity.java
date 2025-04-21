3d39237884b308b041191deecea52d1428196df4
BlokadaVN <barracudavnk@gmail.com>
2025-04-21 13:23:36 +0700

    xo

---
+++ XO/app/src/main/java/com/example/xo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.xo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButtonPvP = findViewById(R.id.imageButtonPvP);
+        ImageButton imageButtonPvC = findViewById(R.id.imageButtonPvC);
+
+        imageButtonPvP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ActivityTwoPlayer.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
