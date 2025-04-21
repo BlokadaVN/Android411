ddc267a2c4ee829c1e2b7512481fb772f6aa6e79
BlokadaVN <barracudavnk@gmail.com>
2025-04-21 15:12:04 +0700

    xo1

--- XO/app/src/main/java/com/example/xo/ActivityTwoPlayer.java
+++ XO/app/src/main/java/com/example/xo/ActivityTwoPlayer.java
@@ -1,6 +1,11 @@
 package com.example.xo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,26 @@ public class ActivityTwoPlayer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_player);
 
+        EditText playerOne = findViewById(R.id.playerOne);
+        EditText playerTwo = findViewById(R.id.playerTwo);
+        ImageButton imageButtonEasy = findViewById(R.id.imageButtonEasy);
+        ImageButton imageButtonHard = findViewById(R.id.imageButtonHard);
 
+        imageButtonEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String getPlayerOneName = playerOne.getText().toString();
+                String getPlayerNameTwo = playerTwo.getText().toString();
+
+                if (getPlayerOneName.isEmpty() || getPlayerNameTwo.isEmpty()) {
+                    Toast.makeText(ActivityTwoPlayer.this, "Enter name player", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(ActivityTwoPlayer.this, ActivityTwoPlayerEasy.class);
+                    intent.putExtra("p1", getPlayerOneName);
+                    intent.putExtra("p2", getPlayerNameTwo);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
